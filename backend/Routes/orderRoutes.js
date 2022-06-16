d78bb6bbda02bc65c1b76f073633003a20c4814b
risshi-dev <2019ucp1415@mnit.ac.in>
2022-06-16 16:59:22 +0530

    order api fix

--- backend/Routes/orderRoutes.js
+++ backend/Routes/orderRoutes.js
@@ -1,134 +1,110 @@
 import express from "express";
-import tokenGenerator from '../jwtgenerator.js'
+import tokenGenerator from "../jwtgenerator.js";
 import Order from "../models/orderModel.js";
 import aSH from "express-async-handler";
 import { protect } from "../middleware/authMiddleware.js";
-import Razorpay from 'razorpay'
+import Razorpay from "razorpay";
 import mailer from "../mailer.js";
 const orderRouter = express.Router();
 
 orderRouter.post(
-	"/create_order_razorpay",
-    protect,
-	aSH(async (req, res) => {
+  "/create_order_razorpay",
+  protect,
+  aSH(async (req, res) => {
+    var instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
 
-        var instance = new Razorpay({
-            key_id: process.env.RAZORPAY_KEY,
-            key_secret: process.env.RAZORPAY_SECRET,
-          });
-        
-        const {totalPrice} = req.body;
+    const { totalPrice } = req.body;
 
-        var options = {
-            amount: totalPrice*100,  // amount in the smallest currency unit
-            currency: "INR",
-          };
+    var options = {
+      amount: totalPrice * 100, // amount in the smallest currency unit
+      currency: "INR",
+    };
 
-          instance.orders.create(options, function(err, order) {
-            const {id} = order
-          
-        if(!err)
-            res.status(201).json(id)
-        else 
-          throw new Error(JSON.stringify(err))
-          });
+    instance.orders.create(options, function (err, order) {
+      const { id } = order;
 
-
-		
-	})
+      if (!err) res.status(201).json(id);
+      else throw new Error(JSON.stringify(err));
+    });
+  })
 );
 
 orderRouter.post(
-	"/create_order",
-    protect,
-	aSH(async (req, res) => {
-        
-        const orderDetails = req.body;
+  "/create_order",
+  protect,
+  aSH(async (req, res) => {
+    const orderDetails = req.body;
 
-        const order = await Order.create(orderDetails)
+    const order = await Order.create(orderDetails);
 
-        if(order){
-              res.status(201).json(order);
-              mailer(req.user.email,
-                'Your order has been placed',
-                `Your order for ${orderDetails.orderItems.map(order => order.name)} has been placed.`)
-            }
-        else
-        res.status(400)
-        throw new Error('Internal Server Error')
-
-		
-	})
+    if (order) {
+      res.status(201).json(order);
+      mailer(
+        req.user.email,
+        "Your order has been placed",
+        `Your order for ${orderDetails.orderItems.map(
+          (order) => order.name
+        )} has been placed.`
+      );
+    } else res.status(400);
+    throw new Error("Internal Server Error");
+  })
 );
 
 orderRouter.get(
-	"/get_orders",
-    protect,
-	aSH(async (req, res) => {
-        
+  "/get_orders",
+  protect,
+  aSH(async (req, res) => {
+    const order = await Order.find({ user: req.user._id }).populate(
+      "user",
+      "name email"
+    );
 
-        const order = await Order.find({}).populate('user', 'name email');
-
-        if(order)
-              res.status(201).json(order);
-        else
-        res.status(404)
-        throw new Error('Order Not Found')
-
-		
-	})
+    if (order) res.status(201).json(order);
+    else res.status(404);
+    throw new Error("Order Not Found");
+  })
 );
 
 //fetch single product
 
 orderRouter.get(
-	"/get_order/:id",
-    protect,
-	aSH(async (req, res) => {
-        
+  "/get_order/:id",
+  protect,
+  aSH(async (req, res) => {
+    const order = await (
+      await Order.findById(req.params.id).populate("user", "name email")
+    ).execPopulate();
 
-        const order = await (await Order.findById(req.params.id).populate('user', 'name email')).execPopulate();
-
-        if(order)
-              res.status(201).json(order);
-        else
-        res.status(404)
-        throw new Error('Order Not Found')
-
-		
-	})
+    if (order) res.status(201).json(order);
+    else res.status(404);
+    throw new Error("Order Not Found");
+  })
 );
-
 
 orderRouter.post(
-	"/orderPaid/:id",
-    protect,
-	aSH(async (req, res) => {
-        
+  "/orderPaid/:id",
+  protect,
+  aSH(async (req, res) => {
+    const order = await Order.findById(req.params.id);
 
-        const order = await Order.findById(req.params.id)
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentDetails = {
+        id: req.body.id,
+        update_time: Date.now(),
+      };
 
-        if(order){
-              order.isPaid = true;
-              order.paidAt = Date.now()
-              order.paymentDetails = {
-                  id:req.body.id,
-                  update_time: Date.now()
-              }
+      const updatedOrder = await order.save();
 
-              const updatedOrder = await order.save()
-
-              res.json(updatedOrder)
-              }
-
-              
-        else
-        res.status(404)
-        throw new Error('Order Not Found')
-
-		
-	})
+      res.json(updatedOrder);
+    } else res.status(404);
+    throw new Error("Order Not Found");
+  })
 );
 
-
-export default orderRouter
+export default orderRouter;
--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,13 @@ import cors  from 'cors'
 import {notFound, errorHandler} from './middleware/errormiddleware.js'
 import orderRouter from './Routes/orderRoutes.js';
 import path from 'path'
+
 dotenv.config()
+
 const app = express()
+
 app.use(express.json())
+
 app.use(cors())
 
 connectDb()
@@ -20,26 +24,16 @@ app.use((req, res, next)=>{
 })
 
 
+app.get('/', (req,res)=>{
+    res.send("Api is running")
+})
+
 
 app.use('/api/products',router)
 app.use('/api/users',userrouter )
 app.use('/api/orders', orderRouter)
 
 app.get('/api/payment_settings', (req,res) => res.json(process.env.RAZORPAY_KEY))
-
-const __dirname = path.resolve()
-
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-    app.get('*', (req,res) => res.sendFile(path.resolve('frontend', 'build', 'index.html')))
-}
-else{
-    app.get('/', (req,res)=>{
-        res.send("Api is running")
-    })
-}
-
 
 app.use(notFound)
 
