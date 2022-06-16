a6553fd03a6aad475e6e729541ce65b4ce660171
risshi-dev <2019ucp1415@mnit.ac.in>
2022-06-16 17:13:54 +0530

    api update

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,29 +1,28 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-const mailer = (email,sub, text) =>{
-    var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS
-  }
-});
+const mailer = (email, sub, text) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
 
-var mailOptions = {
-  from: process.env.EMAIL,
-  to: email,
-  subject: sub,
-  text: text
+  var mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: sub,
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      //console.log('Email sent: ' + info.response);
+    }
+  });
 };
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-
-}
 
 export default mailer;
